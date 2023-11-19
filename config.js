46403714ef5fcd4d6bd3dc496b4d25a7e421c9d1
arthurdoyle1 <150232794+arthurdoyle1@users.noreply.github.com>
2023-11-20 01:49:52 +1000

    Beta 1

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const config = {
   accessToken:
     'pk.eyJ1IjoiYXJ0ZG95MyIsImEiOiJjbG96bnN4dGEwMmNnMmltajcybzBqYTRrIn0.ZbzBgIBqibfEWXo9j0mUgw',
   CSV: './Sample_Data.csv',
-  center: [ 174.4068, -14.9244],
-  zoom: 6,
+  center: [ 170.4068, -14.9244],
+  zoom: 5.5,
   title: 'Defence Diplomacy Interactive Map Tracker',
   description:
     'Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.',
@@ -40,17 +40,6 @@ const config = {
       listItems: ['Arms control', 'Cultural Diplomacy (Defence)', 'Defence Cooperation', 'Defence Infrastructure', 'HADR – Disaster Response', 'Maritime Security', 'Military Exercises', 'Military Medical Diplomacy', 'MIL-POL Engagement', 'Sports Diplomacy (Defence)', 'Training', 'Visit Diplomacy', ], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
     },
     {
-      type: 'checkbox',
-      title: 'Recieving Countries: ',
-      columnHeader: 'Receiving_Countries',
-      listItems: [
-        'Fiji',
-        'Papua New Guinea',
-        'Samoa',
-        'Solomon Islands',
-        'Tonga',
-        'Vanuatu',
-      ],
     },
   ],
 };
