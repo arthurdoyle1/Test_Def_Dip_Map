019136c2287c3f6c8391503a6502f9a81d3a371a
arthurdoyle1 <150232794+arthurdoyle1@users.noreply.github.com>
2024-01-03 11:11:38 +1000

    didnt work revert

--- config.js
+++ config.js
@@ -2,6 +2,9 @@
 
 // eslint-disable-next-line no-unused-vars
 const config = {
+  style: 'mapbox://styles/mapbox/light-v10',
+  accessToken:
+    'pk.eyJ1IjoiYXJ0ZG95MyIsImEiOiJjbG96bnN4dGEwMmNnMmltajcybzBqYTRrIn0.ZbzBgIBqibfEWXo9j0mUgw',
   CSV: './Sample_Data.csv',
   center: [ 170.4068, -14.9244],
   zoom: 5.5,
