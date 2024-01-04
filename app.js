ae63eb7d977d22cdecccb9ee2b4f45fc82c78f05
arthurdoyle1 <150232794+arthurdoyle1@users.noreply.github.com>
2024-01-04 13:20:48 +1000

    reactive edit

--- app.js
+++ app.js
@@ -612,3 +612,10 @@ document.getElementById('closeHelp').addEventListener('click', function() {
   document.getElementById('helpManual').style.display = 'none';
 });
 
+document.getElementById('legendButton').addEventListener('click', function() {
+  document.getElementById('legendManual').style.display = 'block';
+});
+
+document.getElementById('closeLegend').addEventListener('click', function() {
+  document.getElementById('legendManual').style.display = 'none';
+});
