e2d69a97ae31571a4e19e4a319fe12ca135e5d29
arthurdoyle1 <doyleart@protonmail.com>
2024-01-27 07:54:13 +1000

    Update config.js

--- config.js
+++ config.js
@@ -36,19 +36,6 @@ const config = {
       columnHeader: 'Diplomacy_category', // Case sensitive - must match spreadsheet entry
       listItems: ['Arms control', 'Public Diplomacy', 'Cultural Diplomacy (Defence)', 'Defence Cooperation', 'Defence Infrastructure', 'HADR – Disaster Response', 'Maritime Security', 'Military Exercises', 'Military Medical Diplomacy', 'MIL-POL Engagement', 'Sports Diplomacy (Defence)', 'Training', 'Visit Diplomacy', ], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
     },
-    {
-      type: 'checkbox',
-      title: 'Broad Data Points: ',
-      columnHeader: 'Colour', // Case sensitive - must match spreadsheet entry
-      listItems: ['Broad',]
-    },
-    {
-      type: 'checkbox',
-      id: 'Broad',
-      title: 'Specific Data Points: ',
-      columnHeader: 'Colour', // Case sensitive - must match spreadsheet entry
-      listItems: ['Specific',]
-    },
   ],
 };
 
